3555b5cc3663b0ec5a7a251b063eeebe63943e17
rafghif <95651421+rafghif@users.noreply.github.com>
2022-01-13 20:02:29 +0700

    Input_nilai.py

--- Input_nilai.py
+++ Input_nilai.py
@@ -1,84 +1,38 @@
-from view.input_nilai import *
+# Menginput data
+def input_nama():
+    print("\nMasukkan data mahasiswa")
+    print("...")
+    global nama
+    nama = input("\nNama: ")
+    return nama
 
-data = {}
 
-# Menambahkan data
-def tambah_data():
-    global data
-    ulangi = 'y'
-    while ulangi =='y':
-        nama = input_nama()
-        nim = input_nim()
-        nilai_tugas = input_ntugas()
-        nilai_uts = input_nuts()
-        nilai_uas = input_nuas()
-        nilai_akhir = nakhir()
-        data[nama] = [nama, nim, nilai_tugas, nilai_uts, nilai_uas, nilai_akhir]
-        ulangi = (input('tambah data?(y/t) : '))
+def input_nim():
+    global nim
+    nim = input("NIM: ")
+    return nim
 
-        if ulangi == 't':
-            print('\nData berhasil di tambah!')
-            return data
 
-# Mengubah data
-def ubah_data():
-    nama = input("Masukan nama untuk mengubah data: ")
-    if nama in data.keys():
-        print("\nMau mengubah apa?")
-        sub_data = input("(Semua), (NIM), (Tugas), (UTS), (UAS) : ")
-        if sub_data.lower() == "semua":
-            print("==========================")
-            print("Ubah data {}.".format(nama))
-            print("==========================")
-            data[nama][1] = input("Ubah NIM:")
-            data[nama][2] = int(input("Ubah Nilai Tugas: "))
-            data[nama][3] = int(input("Ubah Nilai UTS: "))
-            data[nama][4] = int(input("Ubah Nilai UAS: "))
-            data[nama][5] = data[nama][2] *30/100 + data[nama][3]*35/100 + data[nama][4] *35/100
-            print("\nBerhasil ubah data!")
+def input_ntugas():
+    global nilai_tugas
+    nilai_tugas = int(input("Masukkan nilai tugas: "))
+    return nilai_tugas
 
-        elif sub_data.lower() == "nim":
-            data[nama][1] = input("\nNIM:")
-            print('\nData berhasil di ubah!')
-        elif sub_data.lower() == "tugas":
-            data[nama][2] = int(input("\nNilai Tugas: "))
-            data[nama][5] = data[nama][2] *30/100 + data[nama][3]*35/100 + data[nama][4] *35/100
-            print('\nData berhasil di ubah!')
-        elif sub_data.lower() == "uts":
-            data[nama][3] = int(input("\nNilai UTS: "))
-            data[nama][5] = data[nama][2] *30/100 + data[nama][3]*35/100 + data[nama][4] *35/100
-            print('\nData berhasil di ubah!')
-        elif sub_data.lower() == "uas":
-            data[nama][4] = int(input("\nNilai UAS: "))
-            data[nama][5] = data[nama][2] *30/100 + data[nama][3]*35/100 + data[nama][4] *35/100
-            print('\nData berhasil di ubah!')
-        else:
-            print("\nmenu tidak ditemukan.")
 
-    else:
-        print("'{}' tidak ditemukan.".format(nama))
+def input_nuts():
+    global nilai_uts
+    nilai_uts = int(input("Masukkan nilai UTS: "))
+    return nilai_uts
 
-# Menghapus data
-def hapus_data():
-    nama = input("Masukan nama untuk menghapus data : ")
-    if nama in data.keys():
-        del data[nama]
-        print("\nData '{}' berhasil dihapus.".format(nama))
-    else:
-        print("'{}' tidak ditemukan.".format(nama))
 
-# Mencari data
-def cari_data():
-    print("Mencari data: ")
-    print("=================================================")
-    nama = input("Masukan nama untuk mencari data: ")
-    print('\nResult')
-    print("==============================================================")
-    print("|      Nama      |    NIM    | Tugas |  UTS  |  UAS  | Akhir |")
-    print("==============================================================")
-    if nama in data.keys():
-        print("| {0:14} | {1:9} | {2:5} | {3:5} | {4:5} | {5:5}"
-            .format(nama, data[nama][1], data[nama][2], data[nama][3], data[nama][4], data[nama][5]))
-        print('--------------------------------------------------------------')
-    else:
-        print("'{}' tidak ditemukan.".format(nama))
+def input_nuas():
+    global nilai_uas
+    nilai_uas = int(input("Masukkan nilai UAS: "))
+    return nilai_uas
+
+
+# Nilai akhir
+def nakhir():
+    global nilai_akhir
+    nilai_akhir = (nilai_tugas)*30/100 + (nilai_uts)*35/100 + (nilai_uas)*35/100
+    return nilai_akhir
